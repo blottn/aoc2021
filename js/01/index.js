92d5b9528507c04eef3ab17208e955f316974c46
blottn <blottn@arista.com>
2021-12-01 13:44:56 +0000

    day 01 - part 02

--- js/01/index.js
+++ js/01/index.js
@@ -2,5 +2,14 @@ import readInput, {readInputFile} from "../util/input.js";
 
 let ls = await readInputFile("./input");
 ls = ls.map(i => parseInt(i));
-let o = ls.reduce(([incs, prev], next) => next > prev ? [incs + 1, next] : [incs, next], [0, ls[0]]);
-console.log(o);
+
+let count = 0;
+for (let i = 3; i < ls.length; i += 1) {
+    let sum = ls[i] + ls[i - 1] + ls[i - 2];
+    let prev = ls[i - 1] + ls[i - 2] + ls[i - 3];
+    console.log(`sum: ${sum} prev: ${prev}`);
+    if (sum > prev) {
+        count += 1;
+    }
+}
+console.log(count);
